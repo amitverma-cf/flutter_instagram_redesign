4543271ca9839a26eaf38986323b8288c6141eb2
Amit Verma <ava.amitverma@gmail.com>
2020-09-13 00:14:50 +0530

    flutter_redesign_instagram

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Redesign Instagram ',
+      title: 'Flutter Instagram Redesign',
       theme: ThemeData(
         textTheme: GoogleFonts.muliTextTheme(),
         primarySwatch: Colors.pink,
